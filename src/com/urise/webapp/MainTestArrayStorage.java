fc1e204469e4938e781986b44d8daa3db84c41e5
Александр <aldrpavlov0714@gmail.com>
2020-12-11 12:05:36 +1000

    Home Work 3 (after task analysis in the lesson video)

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -1,7 +1,6 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.ArrayStorage;
 import com.urise.webapp.storage.SortedArrayStorage;
 import com.urise.webapp.storage.Storage;
 
